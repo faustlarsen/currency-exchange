ad7b5d356b70fdb28cb80afc1595e7ed7aab96d8
Constantine <faustlarsen@Constantines-MacBook-Pro-2.local>
2020-11-20 11:59:31 -0700

    add 5 currencies, async function, fix typos

--- src/exchange.js
+++ src/exchange.js
@@ -1,9 +1,9 @@
 export async function exchange() {
   try {
-    let respone = await fetch(`https://v6.exchangerate-api.com/v6/${[process.env.API_KEY]}/latest/USD`);
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/${[process.env.API_KEY]}/latest/USD`);
     let jsonifiedResponse;
     if (response.ok && response.status == 200) {
-      jsonifiedResponse = await respone.json();
+      jsonifiedResponse = await response.json();
     } else {
       jsonifiedResponse = false;
     }
--- src/main.js
+++ src/main.js
@@ -4,8 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import { exchange } from './exchange.js';
 
+async function newRates(newCurrency) {
+  let jsonCurrency = await exchange(newCurrency);
+  if (jsonCurrency === false) {
+    $("#result").text("Sorry, this currency isn't available")
+  } else {
+    $("#result").text(`You have ${jsonCurrency}`)
+  }
+}
 
 
 $(document).ready(function() {
-
-}
+  $("#submit").click(function() {
+    event.preventDefault();
+    let newCurrency = $("#currency").val();
+    newRates(newCurrency)
+  });
+});
